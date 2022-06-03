45dd1973fd96b026b430b5aa581102e1accd2b3c
Andrew G <guzmanandrewe@gmail.com>
2022-06-03 13:47:58 -0700

    Add playRound function which declares a winner

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ function computerPlay() {
     let randomSelection = rockPaperScissors[random]
     return randomSelection
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    
+    if ((playerSelection === 'scissors' && computerSelection === 'rock') || 
+        (playerSelection === 'rock' && computerSelection === 'paper') || 
+        (playerSelection === 'paper' && computerSelection === 'scissors')) {
+            return `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
+        } else if (
+            (playerSelection === 'rock' && computerSelection === 'scissors') || 
+            (playerSelection === 'paper' && computerSelection === 'rock') || 
+            (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`
+        } else if (playerSelection === computerSelection) {
+            return `Tie!`
+        }
+
+}
+
+function capitalize(selection) {
+    let firstChar = selection[0].toUpperCase()
+    let remainingString = selection.slice(1)
+    return firstChar + remainingString
+}
+
+const playerSelection = "rock"
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
