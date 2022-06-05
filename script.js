72142f31142cedf6a4dabadb09cba74d2b11b6b0
Andrew G <guzmanandrewe@gmail.com>
2022-06-05 09:08:49 -0700

    Capitalize strings array in computerPlay() fcn The array started as all lowercase to help make the comparisons between the computerSelection and playerSelection(case-insensitive). In order to display the message as required, another function was created to capitalize the first letter of the playerSelection string. This function was also applied for the computerSelection string but is unnecessary now that the array of choices has been capitalized and the values have been updated in the playRound() function.

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 function computerPlay() {
-    let rockPaperScissors = ['rock', 'paper', 'scissors']
+    let rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
    
-    let max = Math.floor(rockPaperScissors.length - 1)
-    let random = Math.floor(Math.random() * (max + 1))
-    let randomSelection = rockPaperScissors[random]
-    return randomSelection
+    let max = Math.floor(rockPaperScissors.length - 1);
+    let random = Math.floor(Math.random() * (max + 1));
+    let randomSelection = rockPaperScissors[random];
+    return randomSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase();
     
-    if ((playerSelection === 'scissors' && computerSelection === 'rock') || 
-        (playerSelection === 'rock' && computerSelection === 'paper') || 
-        (playerSelection === 'paper' && computerSelection === 'scissors')) {
-            return `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
+    if ((playerSelection === 'scissors' && computerSelection === 'Rock') || 
+        (playerSelection === 'rock' && computerSelection === 'Paper') || 
+        (playerSelection === 'paper' && computerSelection === 'Scissors')) {
+            return `You lose! ${computerSelection} beats ${capitalize(playerSelection)}`;
         } else if (
-            (playerSelection === 'rock' && computerSelection === 'scissors') || 
-            (playerSelection === 'paper' && computerSelection === 'rock') || 
-            (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`
-        } else if (playerSelection === computerSelection) {
-            return `Tie!`
+            (playerSelection === 'rock' && computerSelection === 'Scissors') || 
+            (playerSelection === 'paper' && computerSelection === 'Rock') || 
+            (playerSelection === 'scissors' && computerSelection === 'Paper')) {
+            return `You win! ${capitalize(playerSelection)} beats ${computerSelection}`;
+        } else {
+            return `Tie!`;
         }
 
 }
 
+/* Capitalizes first letter of the player's selection to display in winning message */
 function capitalize(selection) {
-    let firstChar = selection[0].toUpperCase()
-    let remainingString = selection.slice(1)
-    return firstChar + remainingString
+    let firstChar = selection[0].toUpperCase();
+    let remainingString = selection.slice(1);
+    return firstChar + remainingString;
 }
 
-const playerSelection = "rock"
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+const playerSelection = "paper";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
