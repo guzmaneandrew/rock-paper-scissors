59c811e626c0babe94013b6dcfe115406a2f544e
Andrew G <guzmanandrewe@gmail.com>
2022-07-25 11:50:15 -0700

    Add game function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 * Function that makes the computer play
 * @return {string} random string from rockPaperScissors array
 */
-function computerPlay() {
+function getComputerChoice() {
     let rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
    
     let max = Math.floor(rockPaperScissors.length - 1);
@@ -23,16 +23,51 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection === 'scissors' && computerSelection === 'Rock') || 
         (playerSelection === 'rock' && computerSelection === 'Paper') || 
         (playerSelection === 'paper' && computerSelection === 'Scissors')) {
-            return `You lose! ${computerSelection} beats ${capitalize(playerSelection)}`;
+            //return `You lose! ${computerSelection} beats ${capitalize(playerSelection)}`;
+            return -1;
         } else if (
             (playerSelection === 'rock' && computerSelection === 'Scissors') || 
             (playerSelection === 'paper' && computerSelection === 'Rock') || 
             (playerSelection === 'scissors' && computerSelection === 'Paper')) {
-            return `You win! ${capitalize(playerSelection)} beats ${computerSelection}`;
-        } else {
-            return `Tie!`;
+            //return `You win! ${capitalize(playerSelection)} beats ${computerSelection}`;
+            return 1;
+        } else if (
+            (playerSelection === 'scissors' && computerSelection === 'Scissors') || 
+            (playerSelection === 'rock' && computerSelection === 'Rock') || 
+            (playerSelection === 'paper' && computerSelection === 'Paper')) {
+            //return `Tie!`;
+            return 0;
         }
 
+}
+
+/* 
+* Function that plays a 5 round game, keeps score, and reports a winner at the end.
+* @params none
+* @returns {string} string indicating winner of the game
+*/
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    for(let i = 0; i < 5; i++) {
+        let userSelection = prompt("Rock, paper, or scissors?");
+        let computerSelection = getComputerChoice();
+        let result = playRound(userSelection, computerSelection);
+
+        if(result === 1) {
+            userScore++;
+        } else if(result === -1) {
+            computerScore++;
+        }
+    }
+    
+    if(userScore > computerScore) {
+        return `You won the game!`;
+    } else if (userScore < computerScore) {
+        return `You lost the game!`;
+    } else {
+        return `No winners, it's a tie!`;
+    }
 }
 
 /* 
@@ -42,10 +77,8 @@ function playRound(playerSelection, computerSelection) {
 */
 function capitalize(selection) {
     let firstChar = selection[0].toUpperCase();
-    let remainingString = selection.slice(1);
+    let remainingString = selection.slice(1).toLowerCase();
     return firstChar + remainingString;
 }
 
-const playerSelection = "paper";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+game();
